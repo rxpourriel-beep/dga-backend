fb8c106e6d5c70071c4f5ad59588f2a4ee791421
rxpourriel-beep <rxpourriel@gmail.com>
2026-04-19 00:39:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ import json
 import tempfile
 import traceback
 import numpy as np
+
+import matplotlib
+matplotlib.use('Agg)
+               
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
